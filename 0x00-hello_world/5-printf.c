71b9285bac853e9d731f00d8990e755004d34715
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-05 19:18:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - start of the program
+ * Return: always returns 0 meaning successful termination
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
