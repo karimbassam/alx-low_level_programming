0112b8ad2c707d3439438953a62ff335a6b56f94
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-06 06:37:42 -0700

    101-qoute.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,16 @@
 #include <unistd.h>
 /**
  * main - is the start of the program
+ *
+ * Description: print a qoute using the write(); function
+ *
  * Return: 1 means successful termination
  */
+
 int main(void)
 {
 	char qoute[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	
 	write(1, qoute, 59);
-
-	return (0);
+	return (1);
 }
