4a811c3419ef41b4dd4e6f78cdf742501f878bed
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-05 19:12:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 *main - main entry of the program
-*return: it always returns 0 which means successful progrma termination
+*Return: it always returns 0 which means successful progrma termination
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
