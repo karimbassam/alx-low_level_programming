19787b05975fa666d97a2830e9d544492ed37530
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-10 17:44:12 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,14 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
+	lastDigit = n % 10;
 	if (n < 0)
 	{
-		lastDigit = n % 10;
 		lastDigit = lastDigit * -1;
+	}
+	else
+	{
+		lastDigit = lastDigit;;
 	}
 	_putchar(lastDigit + '0');
 
