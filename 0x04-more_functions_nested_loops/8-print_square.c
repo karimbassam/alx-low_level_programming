2f57a9148d401629b72904e0610164e50a2cf4a8
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-12 18:11:55 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,12 @@ void print_square(int size)
 {
 	int row, col;
 
-	for (row = 1; row <= size; row++)
+	for (row = 0; row < size; row++)
 	{
-		for (col = 1; col <= size; col++)
+		for (col = 0; col < size; col++)
+		{
 			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
