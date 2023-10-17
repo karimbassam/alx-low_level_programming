53e1fe2276037263ad723e03796dd18036c556ae
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-16 19:01:32 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse given string
+ *
+ * @s: string parameter
+ *
+ * Return: void type
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char tmp;
+
+	for (i = 0; s[i] != '\0'; ++i);
+
+	for (j = 0; j < i / 2; j++)
+	{
+		tmp = s[j];
+		s[j] = s[i - 1 - j];
+		s[i - 1 - j] = tmp;
+	}
+}
