a0219cc2ff79d86a1df2bedf1a02400a2d4f0152
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-23 17:18:23 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints sum of two diagonals
+ *
+ * @a: pointer to 2d array of integers
+ * @size: parameter to array size
+ *
+ * Return: void type
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, s1 = 0, s2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		s1 += a[i];
+		s2 += a[size - i - 1];
+		a += size;
+	}
+	printf("%d, ", s1);
+	printf("%d\n", s2);
+}
