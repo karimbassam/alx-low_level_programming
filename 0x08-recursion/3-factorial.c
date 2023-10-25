57d55e82385db8121d88cff46a9887f8d01d2f56
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-25 14:05:45 -0700

    ./3-factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * factorial - finds factorial of number
+ *
+ * @n: integer
+ *
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n == 0)
+		return (1);
+
+	return (n * factorial(n - 1));
+}
