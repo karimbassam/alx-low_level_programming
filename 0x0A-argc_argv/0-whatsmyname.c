a565f860485950c4685e37eba191b3876a691f45
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-10-27 19:53:08 -0700

    argc, argv$ mv mynameis mynewnameis

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name followed by new line
+ *
+ * @argc: length
+ * @argv: array
+ *
+ * Return: 0 susccess
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
