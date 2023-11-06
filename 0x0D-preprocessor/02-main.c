c0c990a8cff73a757f3c7f388c8af96618d9ce6a
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-11-06 14:14:46 -0800

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function of the progrm prints file name followed by new line
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
