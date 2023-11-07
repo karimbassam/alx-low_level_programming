7fa7765f750161427d812996733d099ebd5ae5a0
karimbassam <karim.bassam.elkhateeb@gmail.com>
2023-11-06 17:09:09 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_STRUCT_H
+#define DOG_STRUCT_H
+
+/**
+ * struct dog - struct of dog
+ * @name: variable of type char
+ * @age: variable of type float
+ * @owner: owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_STRUCT_H */
