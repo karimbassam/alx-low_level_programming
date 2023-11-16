bd2e1f2eb3739ecf5f642068f68f8e607e6808ad
KB <karim.bassam.elkhateeb@gmail.com>
2023-11-16 19:24:58 +0200

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * pre_main_message - Prints the pre-main messages.
+ */
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
