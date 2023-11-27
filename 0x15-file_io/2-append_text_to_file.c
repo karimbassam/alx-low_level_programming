70a79fddd9c0ee3e7b08834a687c80945e76825c
KB <karim.bassam.elkhateeb@gmail.com>
2023-11-27 23:03:13 +0200

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+ * _strlen - returns the length of string
+ * @s: the string to check length
+ *
+ * Return: integer length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
 
 /**
  * append_text_to_file - Appends text at the end of a file.
@@ -9,7 +25,8 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, written_bytes, close_status;
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
@@ -18,20 +35,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1); /* Error opening file for append */
 
-	if (text_content != NULL)
-	{
-		written_bytes = write(fd, text_content, sizeof(char));
-		close_status = close(fd);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
 
-		if (written_bytes == -1 || close_status == -1)
-			return (-1); /* Error writing to or closing file */
-	}
+	if (bytes == len)
+		return (1);
 	else
-	{
-		close_status = close(fd);
-		if (close_status == -1)
-			return (-1); /* Error closing file */
-	}
-
-	return (1);
+		return (-1);
 }
