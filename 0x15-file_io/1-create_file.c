ce116d821fedc9013a4839065655742f806e701f
KB <karim.bassam.elkhateeb@gmail.com>
2023-11-27 22:51:51 +0200

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * _strlen -returns the length of string
+ * @s: the string to check length
+ *
+ * Return: integer of string length
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
  * create_file - Creates a file with the specified content.
  * @filename: The name of the file to create.
  * @text_content: The content to write to the file.
@@ -9,29 +27,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, written_bytes, close_status;
+	int fd;
+	size_t bytes = 0, len = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
 	if (fd == -1)
 		return (-1); /* Error creating or opening file */
 
-	if (text_content != NULL)
-	{
-		written_bytes = write(fd, text_content, sizeof(char));
-		close_status = close(fd);
+	if (len)
+		bytes = write(fd, text_content, len);
+	close(fd);
 
-		if (written_bytes == -1 || close_status == -1)
-			return (-1); /* Error writing to or closing file */
-	}
+	if (bytes == len)
+		return (1);
 	else
-	{
-		close_status = close(fd);
-		if (close_status == -1)
-			return (-1); /* Error closing file */
-	}
-
-	return (1);
+		return (-1);
 }
