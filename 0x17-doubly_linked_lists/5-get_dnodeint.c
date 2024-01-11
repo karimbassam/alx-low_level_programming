5f56da3d5ee7c34d3c4f6853604474593987ba51
KB <karim.bassam.elkhateeb@gmail.com>
2024-01-11 13:19:11 +0200

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,19 +11,19 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 	dlistint_t *current = head;
-	
+
 	/* Traverse the list to the desired index */
 	for (i = 0; i < index && current != NULL; i++)
 	{
 		current = current->next;
 	}
-	
+
 	/* Check if the desired index is within the list bounds */
 	if (i == index && current != NULL)
 	{
-		return current;
+		return (current);
 	}
-	
+
 	/* Return NULL if the node does not exist at the given index */
-	return NULL;
+	return (NULL);
 }
