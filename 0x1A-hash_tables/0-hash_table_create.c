4554ba1b59ab8bb66181697229d8c9a00e10c83c
KB <karim.bassam.elkhateeb@gmail.com>
2024-09-17 12:55:54 +0300

    Ax

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,8 +14,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/* Allocate memory for the hash table structure */
 	new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
+	{
 		return (NULL);
+	}
 
 	/* Allocate memory for the array of pointers */
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
@@ -27,7 +30,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/* Initialize all the pointers in the array to NULL */
 	for (i = 0; i < size; i++)
+	{
 		new_table->array[i] = NULL;
+	}
 
 	new_table->size = size;
 
