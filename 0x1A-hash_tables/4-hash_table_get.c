4fcec2e283fe4d4cec547487877e1288aa1718bb
KB <karim.bassam.elkhateeb@gmail.com>
2024-09-25 17:54:03 +0300

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,26 +5,26 @@
  * @ht: The hash table to look into.
  * @key: The key you are looking for.
  *
- * Return: The value associated with the key, or NULL if the key couldn’t be found.
+ * Return: The value associated with key or NULL if the key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
-    hash_node_t *node;
+	unsigned long int index;
+	hash_node_t *node;
 
-    if (ht == NULL || key == NULL || *key == '\0')
-        return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-    index = key_index((const unsigned char *)key, ht->size);
-    node = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-    while (node != NULL)
-    {
-        if (strcmp(node->key, key) == 0)
-            return (node->value);
-        node = node->next;
-    }
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
 
-    return (NULL);
+	return (NULL);
 }
 
